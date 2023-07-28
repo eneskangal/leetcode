00bf0169e73a689c12f078435bfabbefeb42908c
eneskangal <eneskangal@gmail.com>
2023-07-28 16:02:10 +0300

    new 10 problems
    new 10 problems

---
+++ maximumProduct/script.js
@@ -0,0 +1,10 @@
+let maximumProduct = function(nums){
+    let sortlanMısDizi=nums.sort(function(a, b){return a - b})
+    if(nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]>nums[0]*nums[1]*nums[nums.length-1]){
+        return nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3]
+    }
+    else{
+        return nums[0]*nums[1]*nums[nums.length-1]
+    }
+}
+console.log(maximumProduct([1,2,3]))
